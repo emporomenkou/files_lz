d42159182f9324cb80e9f2ac5136058b78ed7ee8
emporomenkou <noobprohacker@tutamail.com>
2025-11-24 09:47:34 +0300

    defff

--- liondef.py
+++ liondef.py
@@ -27,9 +27,9 @@ def tipwrds(s1) :
         d1[i][1] = d1[i][0] / len(a) * 100    #создали словарь d, где ключ - слово, значение - лист, состоящий из кол-ва таких слов в тексте и его встречаемости в процентах соответственно
     return d1
 
-d1 = {}
 def tiplttrs (s1) :
     s1.replace(' ', '')
+    d1 = {}
     alf = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя' #создали строку, содержащую только буквы русского алфавита
     for i in s1 :
         if i in alf : #если символ строки является русской буквой
@@ -40,17 +40,23 @@ def tiplttrs (s1) :
                 d1[i] = [1, 1 / len(s1) * 100] #то же самое, что и со словами
     return d1
 
+def tablewrds() :
+    table = pd.DataFrame(d)
+    print(table) #выводим таблицу со статистикой по словам
+
+def pltlttrs() :
+    k = d2.keys()
+    v = d2.values()
+    plt.plot(k, v)
+    plt.xlabel("Буква")
+    plt.ylabel("Сколько раз встретилась")
+    plt.title("Гистограмма для букв")
+    plt.show() #вывод статистики букв в заданных параметрах
+
+
+
 s = preobr(doc)
 d = tipwrds(s)
 d2 = tiplttrs(s)
-
-table = pd.DataFrame(d)
-print(table) #выводим таблицу со статистикой по словам
-
-k = d2.keys()
-v = d2.values()
-plt.plot(k, v)
-plt.xlabel("Буква")
-plt.ylabel("Сколько раз встретилась")
-plt.title("Гистограмма для букв")
-plt.show() #вывод статистики букв в заданных параметрах
+tablewrds()
+pltlttrs()
